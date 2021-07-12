e0518e2147d4c1e353969b68ba60f97ed3c518d5
Travis Northcutt <travis@travisnorthcutt.com>
2021-07-12 12:31:24 -0600

    test

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  //   console.log(`The event payload: ${payload}`);
+
+  console.log(payload.pull_request.title);
 } catch (error) {
   core.setFailed(error.message);
 }
