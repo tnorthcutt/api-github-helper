734ecfdf63fd9aa70a3eb9c166a37b4ecacfc805
Travis Northcutt <travis@travisnorthcutt.com>
2021-07-12 12:45:08 -0600

    test

--- dist/index.js
+++ dist/index.js
@@ -6298,7 +6298,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   //   console.log(`The event payload: ${payload}`);
 
-  console.log(payload.pull_request);
+  console.log(payload);
 } catch (error) {
   core.setFailed(error.message);
 }
